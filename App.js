3b86d842231437bf5f3a587285c842033c4864fa
mhamzaerol <mehamerol@gmail.com>
2023-05-22 13:43:40 +0900

    Fix the initial bug

--- App.js
+++ App.js
@@ -2,19 +2,13 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import { Provider } from 'react-redux';
 import CounterComponent from 'src/components/ExampleReduxComponent';
-import makeStore from 'src/ExampleReduxStore';
-
-const store = makeStore();
 
 export default function App() {
   return (
-    <Provider store={store}>
-      <View style={styles.container}>
-        {/* <Text>Open up App.js to start working on your app!</Text> */}
-        {/* <StatusBar style="auto" /> */}
-        <CounterComponent />
-      </View>
-    </Provider>
+    <View style={styles.container}>
+      <Text>Open up App.js to start working on your app!</Text>
+        <StatusBar style="auto" /> 
+    </View>
   );
 }
 
