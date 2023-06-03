7691e5f7bc9bb8ff67db36f5321e77e56b542952
cs20200728 <qiuli@kaist.ac.kr>
2023-06-03 10:12:08 +0900

    new

--- App.js
+++ App.js
@@ -1,28 +1,8 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-import { Provider } from 'react-redux';
-import CounterComponent from 'src/components/ExampleReduxComponent';
-import makeStore from 'src/ExampleReduxStore';
-
-const store = makeStore();
+import React from 'react';
+import Calendar from './src/screens/Calendar.js';  // update this path to wherever your file is located
 
 export default function App() {
   return (
-    <Provider store={store}>
-      <View style={styles.container}>
-        {/* <Text>Open up App.js to start working on your app!</Text> */}
-        {/* <StatusBar style="auto" /> */}
-        <CounterComponent />
-      </View>
-    </Provider>
+    <Calendar />
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
