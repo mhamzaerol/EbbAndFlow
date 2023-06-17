64ba80a2d1df127a36e3990fd4a23df3547036de
Nursultan Soodonbekov <98944933+nursultan2ch@users.noreply.github.com>
2023-06-17 10:45:20 +0900

    Update WriteDiary.js

--- src/screens/WriteDiary.js
+++ src/screens/WriteDiary.js
@@ -138,6 +138,7 @@ const styles = StyleSheet.create({
     flexDirection: "row",
     justifyContent: "space-between",
     alignItems: "center",
+    top:13
   },
   back: {
     flexDirection: "row",
