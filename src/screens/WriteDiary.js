b5fb3d84c0412c8be4621021a815f85c20db2fa4
Nursultan Soodonbekov <98944933+nursultan2ch@users.noreply.github.com>
2023-06-17 10:36:12 +0900

    Update WriteDiary.js

--- src/screens/WriteDiary.js
+++ src/screens/WriteDiary.js
@@ -1,99 +1,110 @@
 import React, { useState } from "react";
 import { View, TextInput, StyleSheet, Text, Image, TouchableHighlight } from "react-native";
-import { AntDesign } from "@expo/vector-icons";
+// import { AntDesign } from "@expo/vector-icons";
+import { GoBackArrowIcon } from "src/components/svg/GoBackArrowIcon";
+import { useDispatch } from "react-redux";
+import { goPrevPage } from "src/redux/actions";
+import { TouchableOpacity } from "react-native";
+import { EraserIcon } from "src/components/svg/EraserIcon";
+import { goNextPage } from "src/redux/actions";
+import { useEffect } from "react";
+import { Keyboard } from "react-native";
+import { TouchableWithoutFeedback } from "react-native";
+
 
 const JournalPage = () => {
   const [title, setTitle] = useState("");
   const [entryText, setEntryText] = useState("");
-  
+
+  const dispatch = useDispatch();
 
   const handleSaveEntry = () => {
-    // Implement the logic to save the entry to a database or storage here
-    // For simplicity, we'll just log the entry text for now
-    console.log("Entry Text:", entryText);
-    // Reset the state after saving
-    setEntryText("");
+    dispatch(goNextPage('Home'));
   };
 
   const handleBack = () => {
-    // Go back to the previous screen
+    dispatch(goPrevPage());
   };
 
   const handleErase = () => {
-    
+    setEntryText("")
   };
 
-  const handleChat = () => {};
+  const handleChat = () => {
+    dispatch(goNextPage('MrSeagull'));
+  };
 
   return (
-    <View style={styles.container}>
-      <View style={{marginBottom:20, top:40}}>
-        <View style={styles.back}>
-          <TouchableHighlight onPress={handleBack}>
-            <AntDesign name="left" size={30}/>
-          </TouchableHighlight>
-          <TouchableHighlight onPress={handleErase}>
+
+    <TouchableWithoutFeedback onPress={Keyboard.dismiss} accessible={false}>
+      <View style={styles.container}>
+        <View style={{ marginBottom: 20, top: 40, marginHorizontal: -10 }}>
+          <View style={styles.back}>
+            <TouchableOpacity onPress={handleBack}>
+              <GoBackArrowIcon />
+            </TouchableOpacity>
+            <TouchableOpacity onPress={handleErase}>
+              <EraserIcon
+              />
+            </TouchableOpacity>
+          </View>
+        </View>
+        <View style={{ flexDirection: "row", marginTop: 20, alignItems: 'center', marginBottom: 15 }}> 
+          <View style={{ marginRight: 5 }}>
+            <Text style={{ fontSize: 20, fontWeight: 'bold' }}>Title:</Text>
+          </View>
+
+          <View style={styles.title}>
+            <TextInput
+              style={{ fontSize: 20,}}
+              placeholder="Write the title here"
+              value={title}
+              onChangeText={(title) => setTitle(title)}
+
+            />
+          </View>
+        </View>
+        <TextInput
+          style={styles.input}
+          multiline
+          placeholder="Write your journal entry here"
+          value={entryText}
+          onChangeText={(text) => setEntryText(text)}
+        />
+
+        {/* <View style={styles.images}>
+        
+        
+      </View> */}
+
+        <View style={styles.button}>
+
+          <TouchableOpacity onPress={handleSaveEntry} style={{ flexDirection: 'column', alignItems: 'center' }}>
             <Image
-          style={{height:35, width:35}}
-          source={require("images/eraser_9.png")}
-        />
-          </TouchableHighlight>
+              style={styles.saveImg1}
+              source={require("images/icons8-boat-96.png")}
+            />
+            <View style={styles.save}>
+              <Text style={{ fontSize: 20 }}>
+                Save & Exit
+              </Text>
+              {/* <Text style={{ fontSize: 20 }}>Exit</Text> */}
+            </View>
+          </TouchableOpacity>
+
+          <TouchableOpacity onPress={handleChat} style={{ flexDirection: 'column', alignItems: 'center' }}>
+            <Image
+              style={styles.saveImg2}
+              source={require("images/icons8-seagull-100.png")}
+            />
+            <View style={styles.talk}>
+              <Text style={{ fontSize: 20 }}>Talk with Mr.</Text>
+              <Text style={{ fontSize: 20 }}>Seagull</Text>
+            </View>
+          </TouchableOpacity>
         </View>
       </View>
-    
-      <View style={{ flexDirection: "row", justifyContent: "space-between", top:15}}>
-        <View style={{bottom:10, left:90}}>
-          <Text style={{ fontSize: 20, }}>Title:</Text>
-        </View>
-
-        <View style={styles.title}>
-          <TextInput
-            style={{ fontSize: 18, right:10 }}
-            placeholder="Write the title here"
-            value={title}
-            onChangeText={(title) => setEntryText(setTitle)}
-           
-          />
-        </View>
-      </View>
-
-      <TextInput
-        style={styles.input}
-        multiline
-        placeholder="Write your journal entry here"
-        value={entryText}
-        onChangeText={(text) => setEntryText(text)}
-      />
-
-      <View style={styles.images}>
-        <Image
-          style={styles.saveImg1}
-          source={require("images/icons8-boat-96.png")}
-        />
-        <Image
-          style={styles.saveImg2}
-          source={require("images/icons8-seagull-100.png")}
-        />
-      </View>
-
-      <View style={styles.button}>
-       
-        <TouchableHighlight onPress={handleSaveEntry}>
-          <View style={styles.save}>
-            <Text style={{ fontSize: 14, top:6, left:3 }}>
-              Save & Exit
-            </Text>
-          </View>
-        </TouchableHighlight>
-
-        <TouchableHighlight onPress={handleChat}>
-          <View style={styles.talk}>
-            <Text style={{ fontSize: 14, top:6, left:3 }}>Talk with</Text>
-            <Text style={{ fontSize: 14, top:6, left:3 }}>Mr.Seagull</Text>
-          </View>
-        </TouchableHighlight>
-      </View>
-    </View>
+    </TouchableWithoutFeedback>
   );
 };
 
@@ -102,44 +113,52 @@ const styles = StyleSheet.create({
     flex: 1,
     padding: 30,
     height: 50,
+    backgroundColor: "white",
   },
   title: {
-    height: 50,
+    // height: 30,
+    // marginBottom: 10,
     width: 220,
     borderColor: "gray",
-    bottom:8,
-  
+    // bottom: 8,
+
   },
   input: {
-    height: 600,
+    flex: 1,
     borderWidth: 1,
     borderColor: "gray",
-    bottom:0,
-    marginBottom: 55,
-    padding: 5,
+    bottom: 0,
+    marginBottom: 10,
+    padding: 10,
     flexDirection: "row",
-    fontSize: 18,
+    fontSize: 16,
   },
   button: {
     fontSize: 50,
     flexDirection: "row",
     justifyContent: "space-between",
-    marginTop: 15,
+    alignItems: "center",
   },
   back: {
     flexDirection: "row",
     marginBottom: 16,
     justifyContent: "space-between",
+    alignItems: "center",
   },
   save: {
-    height: 46,
-    width: 108,
-    borderWidth: 1,
-    borderColor: "gray",
-    paddingTop: 7,
-    paddingLeft: 13,
-    paddingRight: 5,
-    paddingBottom: 5,
+    // height: 46,
+    // width: 108,
+    // paddingTop: 7,
+    // paddingLeft: 13,
+    // paddingRight: 5,
+    // paddingBottom: 5,
+    alignItems: "center",
+    justifyContent: "center",
+    backgroundColor: 'white',
+    borderRadius: 10,
+    padding: 5,
+    borderColor: 'black',
+    borderWidth: 1
   },
   images: {
     height: 35,
@@ -148,25 +167,29 @@ const styles = StyleSheet.create({
     justifyContent: "space-between",
   },
   saveImg1: {
-    bottom:20,
-    width: "20%",
-    height: "160%",
-    marginLeft: "3%",
+    // bottom: 20,
+    // width: "100%",
+    // height: "160%",
+    // marginLeft: "3%",
   },
   saveImg2: {
-    bottom:20,
-    width: "20%",
-    height: "160%",
-    marginRight: "5%",
+    // bottom: 20,
+    // width: "100%",
+    // height: "160%",
+    // marginRight: "5%",
   },
   talk: {
+    // textAlign: "center",
+    alignItems: "center",
+    justifyContent: "center",
+    backgroundColor: 'white',
+    borderRadius: 10,
+    padding: 5,
+    borderColor: 'black',
     borderWidth: 1,
-    borderColor: "gray",
-    textAlign: "center",
-    paddingLeft: 18,
-    fontSize: 14,
-    height: 46,
-    width: 108,
+    // paddingLeft: 18,
+    // height: 46,
+    // width: 108,
   },
 });
 
