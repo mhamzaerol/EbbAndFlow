33b58a7938df50ee929b812864911e50e7ab83e8
mhamzaerol <mehamerol@gmail.com>
2023-06-19 19:23:00 +0900

    Make calendar

--- src/screens/Calendar.js
+++ src/screens/Calendar.js
@@ -1,5 +1,4 @@
 import { StyleSheet, Text, View, Image, SafeAreaView, TouchableOpacity } from 'react-native';
-import Svg, { Path } from "react-native-svg";
 import React, { useState, Component } from 'react';
 import { Calendar, LocaleConfig } from 'react-native-calendars';
 import { Button } from 'react-native';
@@ -7,41 +6,46 @@ import { GoBackArrowIcon } from 'src/components/svg/GoBackArrowIcon';
 import { SettingsIcon } from 'src/components/svg/SettingsIcon';
 import { useDispatch } from 'react-redux';
 import { goNextPage, goPrevPage } from 'src/redux/actions';
-import { useSelector } from 'react-redux'; 
+import { useSelector } from 'react-redux';
 import { setCurDate } from 'src/redux/actions';
-import { MoodRecord } from 'src/redux/datatypes'; 
+import { MoodRecord } from 'src/redux/datatypes';
+import { emotions } from 'src/components/MoodSquare';
+import { Svg, Path } from "react-native-svg";
 
 
 // Calendar
 export function AppCalendar() {
+
   const [selected, setSelected] = useState('');
-  const [selectedRecord, setSelectedRecord] = useState(null);
+  const [selectedMoodRecord, setselectedMoodRecord] = useState(null);
+  const [selectedDiaryRecord, setselectedDiaryRecord] = useState(null);
+
   const dispatch = useDispatch();
 
   const diaryRecords = useSelector(state => state.persistentData.diaryRecords);
   const moodRecords = useSelector(state => state.persistentData.moodRecords);
+
   const markedDates = moodRecords.reduce((acc, record) => {
     const dateString = record.get('date').toISOString().split('T')[0];
-    return { ...acc, [dateString]: { marked: true, dotColor: 'orange' }};
+    return { ...acc, [dateString]: { marked: true, dotColor: 'orange' } };
   }, {});
+
   const handleDayPress = (day) => {
+    const dayDate = new Date(day.dateString);
     setSelected(day.dateString);
-    dispatch(setCurDate(new Date(day.dateString))); 
-    // const date = useSelector(state => state.temporaryData.curDate);
-    // console.log(date)
+    dispatch(setCurDate(dayDate));
+    
     const mood = moodRecords.find(
-      mood => mood.get('date').toISOString().split('T')[0] === day.dateString
+      mood => mood.check('date', dayDate)
     );
-    const record = diaryRecords.find(
-      record => record.get('date').toISOString().split('T')[0] === day.dateString
+    const diary = diaryRecords.find(
+      diary => diary.check('date', dayDate)
     );
-    // setSelectedRecord(record);
-    setSelectedRecord(mood);
+    
+    setselectedMoodRecord(mood);
+    setselectedDiaryRecord(diary);
   };
 
-  // console.log(markedDates)
-  // console.log(diaryRecords.length);
-  // console.log(diaryRecords);
   return (
     <View>
       <Calendar
@@ -58,44 +62,50 @@ export function AppCalendar() {
         }}
       />
       {selected && (
-  <View style={{
-    flexDirection: 'row', 
-    justifyContent: 'space-between',
-    alignItems: 'center', 
-    padding: 20,
-    borderWidth: 1,
-    borderColor: 'gray',
-    backgroundColor: 'white',
-    
-    
-  }}>
-    <View style={{ flexDirection: 'row', alignItems: 'center' }}> 
-      {selectedRecord && 
-        <Image 
-          source={require('../../assets/Face.png')}
-          style={{ width: 30, height: 30, marginRight: 10 }} // Adjust the size as needed
-        />
-      }
-      <View>
-        <Text style={{ marginBottom: 10 , fontSize: 16}}>Date: {selected}</Text>
-        {selectedRecord
-          ? <Text style={{ marginBottom: 10 , fontSize: 16}}>{selectedRecord.get('diaryTitle')}</Text>
-          : <Text style={{ marginBottom: 10 , fontSize: 16}}>No diary yet.</Text>
-        }
-      </View>
-    </View>
-    <TouchableOpacity
-      style={{ backgroundColor: 'white', borderRadius: 10, borderColor: 'black', borderWidth: 1, padding: 10, borderRadius: 5 }}
-      onPress={() => dispatch(selectedRecord ? goNextPage('MoodTracker') : goNextPage('MoodTracker'))}
-    >
-      <Text style={{ color: 'black', fontSize: 20}}>
-        {selectedRecord ? 'View Mood' : 'Track Mood'}
-      </Text>
-    </TouchableOpacity>
-  </View>
-  
-  
-)}
+        <View style={{
+          flexDirection: 'row',
+          justifyContent: 'space-between',
+          alignItems: 'center',
+          padding: 20,
+          borderWidth: 1,
+          borderColor: 'gray',
+          backgroundColor: 'white',
+
+
+        }}>
+          <View>
+            <Text style={{ marginBottom: 10, fontSize: 16 }}>Date: {selected}</Text>
+            <View style={{ flexDirection: 'row'}}>
+              {selectedMoodRecord &&
+                <Svg
+                  height={30}
+                  width={30}
+                  viewBox="0 0 100 100"
+                >
+                  <Path
+                    d={emotions[Math.min(Math.floor(selectedMoodRecord.get('valence') / 20), emotions.length - 1)]}
+                    fill="#000"
+                  />
+                </Svg>
+              }
+              {selectedDiaryRecord
+                ? <Text style={{ marginBottom: 10, fontSize: 16, marginLeft: 4, marginTop: 6}}>{selectedDiaryRecord.get('diaryTitle')}</Text>
+                : <Text style={{ marginBottom: 10, fontSize: 16, marginLeft: 4, marginTop: 6}}>No diary yet.</Text>
+              }
+            </View>
+          </View>
+          <TouchableOpacity
+            style={{ backgroundColor: 'white', borderRadius: 10, borderColor: 'black', borderWidth: 1, padding: 10, borderRadius: 5 }}
+            onPress={() => dispatch(goNextPage('MoodTracker'))}
+          >
+            <Text style={{ color: 'black', fontSize: 20 }}>
+              {selectedMoodRecord ? 'View Mood' : 'Track Mood'}
+            </Text>
+          </TouchableOpacity>
+        </View>
+
+
+      )}
 
     </View>
   );
@@ -104,15 +114,15 @@ export function AppCalendar() {
 function MainApp() {
 
   const dispatch = useDispatch();
-  
+
   return (
-    <SafeAreaView style={{ flex: 1, backgroundColor:'white', }}>
+    <SafeAreaView style={{ flex: 1, backgroundColor: 'white', }}>
       <View style={styles.iconsContainer}>
         <TouchableOpacity
           onPress={() => { dispatch(goPrevPage()) }}>
           <GoBackArrowIcon />
         </TouchableOpacity>
-        <TouchableOpacity 
+        <TouchableOpacity
           onPress={() => dispatch(goNextPage('Settings'))}
         >
           <SettingsIcon />
